90b6e10771880eb21b2657eff78bc1adcef4adb0
agolaurluik <ago.laur@gmail.com>
2026-07-29 18:36:35 +0300

    backend fix

--- backend/modules/board/board.gateway.ts
+++ backend/modules/board/board.gateway.ts
@@ -55,7 +55,7 @@ export class BoardGateway implements OnGatewayInit, OnGatewayConnection, OnGatew
                 const board = await this.boards.createBoardAndPersist(boardId, hostId);
                 
                 if (!board) {
-                    return;
+                    break;
                 }
 
                 const hostUser = new BoardUser();
@@ -69,18 +69,18 @@ export class BoardGateway implements OnGatewayInit, OnGatewayConnection, OnGatew
                     hostId: board.ownerId,
                     boardId: board.id,
                 });
-
+                break;
             case BoardCommand.JOIN:
                 if (!data.id) {
-                    console.log("No id in socket")
-                    return
+                    console.log("No id in socket under JOIN command")
+                    break
                 }
 
                 if (this.boards.hasBoard(data.id)) {
                     const board = this.boards.getBoard(data.id);
 
-                    if (!board) return
-                    if (!data.user) return
+                    if (!board) break
+                    if (!data.user) break
 
                     board.users.add(data.user);
                     socket.join(board.id);
@@ -89,13 +89,16 @@ export class BoardGateway implements OnGatewayInit, OnGatewayConnection, OnGatew
                         username: data.user.username
                     })
                 }
-
+                break;
             case BoardCommand.LEAVE:
-
+                break;
             case BoardCommand.GET:
-
+                break;
             case BoardCommand.DELETE:
-
+                break;
+            default:
+                console.log("BOARD: default, no commands")
+                break;
         }
     }
 
