bd45e4184f7cda183681b547854a73202d40a365
Rando Viimne <rando0011@hotmail.com>
2026-08-04 16:35:08 +0300

    shapes button, looking into objecteditor

--- frontend/src/types/Tool.ts
+++ frontend/src/types/Tool.ts
@@ -4,4 +4,6 @@ export type Tool =
   | 'select'
   | 'stroke'
   | 'circle'
+  | 'line'
+  | 'shapes'
   // | 'arrow'
--- frontend/src/ui/toolbars/BottomToolbar.tsx
+++ frontend/src/ui/toolbars/BottomToolbar.tsx
@@ -1,9 +1,10 @@
-import type { Tool } from '../types/Tool'
+import type { Tool } from '../../types/Tool'
 import './Toolbar.css'
-import RectangleIcon from './icons/RectangleIcon'
-import MoveIcon from './icons/MoveIcon'
-import SelectorIcon from './icons/SelectorIcon'
-import PencilIcon from './icons/PencilIcon'
+import RectangleIcon from '../icons/RectangleIcon'
+import MoveIcon from '../icons/MoveIcon'
+import SelectorIcon from '../icons/SelectorIcon'
+import PencilIcon from '../icons/PencilIcon'
+import ShapesIcon from '../icons/ShapesIcon'
 
 type Props = {
   tool: Tool
@@ -19,6 +20,10 @@ export function BottomToolbar({
     <div className="bottom-toolbar">
       <button className="tool-button" onClick={() => setTool('circle')}>
         <RectangleIcon color={'none'} />
+      </button>
+
+      <button className="tool-button" onClick={() => setTool('shapes')}>
+        <ShapesIcon color={'none'} />
       </button>
 
       <button className="tool-button" onClick={() => setTool('pan')}>
