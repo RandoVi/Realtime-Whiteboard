76460c0adc970d1ba37b952bcea754b60d6a647c
agolaurluik <ago.laur@gmail.com>
2026-08-15 16:33:43 +0300

    svg error fix

--- frontend/src/ui/icons/RectangleIcon.tsx
+++ frontend/src/ui/icons/RectangleIcon.tsx
@@ -4,7 +4,7 @@ type RectangleIconProps = {
 
 const RectangleIcon = ({ color: _color }: RectangleIconProps) => {
     return (
-        <svg viewBox="-6 -6 36.00 36.00" role="img" xmlns="http://www.w3.org/2000/svg" aria-labelledby="rectangleIconTitle" stroke="#000000" stroke-width="1.8" stroke-linecap="square" stroke-linejoin="miter" fill="none" color="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title id="rectangleIconTitle">Rectangle</title> <rect width="18" height="18" x="3" y="3"></rect> </g></svg>
+        <svg viewBox="-6 -6 36.00 36.00" role="img" xmlns="http://www.w3.org/2000/svg" aria-labelledby="rectangleIconTitle" stroke="#000000" strokeWidth="1.8" strokeLinecap="square" strokeLinejoin="miter" fill="none" color="#000000"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <title id="rectangleIconTitle">Rectangle</title> <rect width="18" height="18" x="3" y="3"></rect> </g></svg>
     )
 }
 
